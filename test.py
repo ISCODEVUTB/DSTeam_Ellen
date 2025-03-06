692ac520c07f7fc95e84efb9c100bff71f0c8974
Yosser-C <yosserjulio2@gmail.com>
2025-03-06 00:53:37 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+
+import unittest
+from main import menu, registrar_usuario  
+
+class TestMain(unittest.TestCase):
+    def test_registrar_usuario(self):
+        
+        self.assertTrue(True)  
+
+if __name__ == '__main__':
+    unittest.main()
